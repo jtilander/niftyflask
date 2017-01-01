4f4cce2b8b77a9632d528f402729426ad778ed6f
Jim Tilander <jim@tilander.org>
2017-01-01 14:06:43 -0800

    Only wait on PGSQL if there is a PGPASSWD set

--- etc/init-app.sh
+++ etc/init-app.sh
@@ -11,15 +11,19 @@ if [ -f /app/requirements.txt ]; then
 	pip install --upgrade -r /app/requirements.txt
 fi
 
-
 export PGUSER=$POSTGRES_USER
 export PGPASSWORD=$POSTGRES_PASSWORD
 
-echo -n Waiting for database to come online.
+# If no postgres password has been set, then don't wait for the database to come up
+if [ -z "$PGPASSWORD" ]; then
+	echo "Not waiting for database to come up."
+else
+	echo -n Waiting for database to come online.
 
-set +e
-while ! psql -c '\q' 2> /dev/null; do
-	echo -n .
-	sleep 1
-done
-echo -e "\nDatabase is online."
+	set +e
+	while ! psql -c '\q' 2> /dev/null; do
+		echo -n .
+		sleep 1
+	done
+	echo -e "\nDatabase is online."
+fi
